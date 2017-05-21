273949033784d070555075391114f196f06aebb6
dolphinClown <yanghao9603@163.com>
2017-05-21 15:53:18 +0800

    添加了题目

--- list_demo/ReconstrutList_8.java
+++ list_demo/ReconstrutList_8.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 /**
  * Created by CLAY on 2017/3/2.
+ * Given a singly linked list L: L0 -> L1 ->...-> Ln-1 -> Ln
+ * reorder it to: L0 -> Ln -> L1 -> Ln-1 -> L2 -> Ln-2 ->...
+ * You must do this in-palce without altering the nodes' values.
+ * For example
+ * Given{1,2,3,4},reorder it to{1,4,2,3}
  */
 public class ReconstrutList_8 {
     public static void main(String[] args) {
