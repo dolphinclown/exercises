27c323e18bf12a3162583418481d3134e0cd6bb6
dolphinclown <yanghao9603@163.com>
2017-06-20 17:40:28 +0800

    修改了求二叉树路径练习中的错误

--- tree_demo/FindPath.java
+++ tree_demo/FindPath.java
@@ -1,78 +1,73 @@
 package sword;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
  * Created by CLAY on 2017/3/15.
- * 打印和为目标值的所有路径(到叶节点停止)
+ * 求二叉树中所有和为某一值得路径（到叶节点停止为一条）
  */
 public class FindPath_25 {
     public static void main(String[] args) {
-
-        BinTreeNode t1 = new BinTreeNode(10);
-        BinTreeNode t2 = new BinTreeNode(5);
-        BinTreeNode t3 = new BinTreeNode(12);
-        BinTreeNode t4 = new BinTreeNode(4);
-        BinTreeNode t5 = new BinTreeNode(7);
+        BinaryTreeNode t1 = new BinaryTreeNode(10);
+        BinaryTreeNode t2 = new BinaryTreeNode(5);
+        BinaryTreeNode t3 = new BinaryTreeNode(12);
+        BinaryTreeNode t4 = new BinaryTreeNode(4);
+        BinaryTreeNode t5 = new BinaryTreeNode(7);
 
         t1.left = t2;
         t1.right = t3;
         t2.left = t4;
         t2.right = t5;
 
-        findPath(t1, 22);
+        ArrayList<LinkedList<Integer>> paths = findPath(t1, 22);
+        System.out.println(paths);
     }
 
-    static class BinTreeNode {
+    static class BinaryTreeNode {
         int val;
-        BinTreeNode left;
-        BinTreeNode right;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
 
-        public BinTreeNode(int val) {
+        public BinaryTreeNode(int val) {
             this.val = val;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static void findPath(BinTreeNode root, int aimVal) {
-        if (root == null || aimVal <= 0) {
-            return;
+    public static ArrayList<LinkedList<Integer>>  findPath(BinaryTreeNode root, int aimVal) {
+        if(root == null){
+            return null;
         }
-
-        if (aimVal < root.val) {
-            return;
+        if(aimVal < root.val){
+            return null;
         }
-
-        LinkedList<BinTreeNode> list = new LinkedList<BinTreeNode>();
+        ArrayList<LinkedList<Integer>> paths = new ArrayList<LinkedList<Integer>>();
+        LinkedList<Integer> path = new LinkedList<Integer>();
         int currentSum = 0;
-        BinTreeNode cur = root;
-        BinTreeNode pre = null;
-
-        while (cur != null || !list.isEmpty()) {
-            while (cur != null) {
-                currentSum += cur.val;
-                list.addLast(cur);
-                pre = cur;
-                cur = cur.left;
-            }
-
-            //当前路径和为目标值，并且当前路径所有节点已经加入路径中
-            if (currentSum == aimVal && pre.left == null && pre.right == null) {
-                for (BinTreeNode t : list) {
-                    System.out.print(t.val + " ");
-                }
-                    System.out.println();
-            }
-
-            if (!list.isEmpty()) {
-                cur = list.getLast();
-                if (cur.left == null || cur.right == null) {
-                    currentSum -= list.removeLast().val;
-                }
-                cur = cur.right;
-            }
-        }
+        findPath(root, aimVal, path, paths, currentSum);
+        return paths;
     }
 
+    private static void findPath(BinaryTreeNode root, int aimVal, LinkedList<Integer> path, ArrayList<LinkedList<Integer>> paths, int currentSum) {
+        currentSum += root.val;
+        path.addLast(root.val);
+
+        //若为叶节点，并且当前路径上个的节点和等于输入值，添加此路径到 paths
+        if(currentSum == aimVal && root.left == null && root.right == null){
+            paths.add(new LinkedList<Integer>(path));
+        }
+
+        //如果不是叶节点，遍历它的子节点
+        if (root.left != null) {
+            findPath(root.left, aimVal, path, paths, currentSum);
+        }
+        if (root.right != null) {
+            findPath(root.right, aimVal, path, paths, currentSum);
+        }
+
+        //退回父节点前，在当前路径上删除此节点
+        path.removeLast();
+    }
 }
