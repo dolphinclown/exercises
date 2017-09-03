44ee2dfab6ce147e489e127897e131b315f8a138
dolphinclown <yanghao9603@163.com>
2017-09-03 15:24:16 +0800

    二叉树的第K个结点

---
+++ tree_demo/FindKthNode.java
@@ -0,0 +1,37 @@
+/*
+给定一颗二叉搜索树，请找出其中的第k大的结点。例如， 5 / \ 3 7 /\ /\ 2 4 6 8 中，按结点数值大小顺序第三个结点的值为4。
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+*/
+public class FindKthNode {
+    private static int num;
+    public TreeNode KthNode(TreeNode pRoot, int k){
+        if (pRoot == null || k <= 0) {
+            return null;
+        }
+        num = k;
+        return findKthNode(pRoot);
+    }
+    public TreeNode findKthNode(TreeNode pRoot){
+        TreeNode target = null;
+        if (pRoot.left != null) {
+            target = findKthNode(pRoot.left);
+        }
+        if (target == null) {
+            if (num == 1) {
+                target = pRoot;
+            }
+            num--;
+        }
+        if (target == null && pRoot.right != null) {
+            target = findKthNode(pRoot.right);
+        }
+        return target;
+    }
+}
